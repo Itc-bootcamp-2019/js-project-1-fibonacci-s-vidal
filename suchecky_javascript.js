0d04fa81ad1fd5ced9d9ce7cd1803e32dfabce0a
Vidal Suchecky <sucheckyvidal@gmail.com>
2020-03-12 13:36:46 +0100

    Milestone7

--- suchecky_javascript.js
+++ suchecky_javascript.js
@@ -4,9 +4,6 @@ function removeClass(varName, className) {
 function addClass(varName, className) {
   varName.classList.add(className);
 }
-function changeInnerText(varName, innerText) {
-  varName.innerText = innerText;
-}
 function toggleFunc(varName, className) {
   varName.classList.toggle(className);
 }
@@ -25,36 +22,63 @@ let isValidNumber = input => {
   if (input > 50) {
     addClass(userInput, "input-error");
     addClass(showError, "show-error");
-    changeInnerText(showError, "Can't be larger than 50");
+    showError.innerText = "Can't be larger than 50";
     return false;
-  } else {
-    return true;
   }
+  return true;
 };
-
+let serverFiboResults = () => {
+  let showAllData = document.getElementById("showAllData");
+  let spinner2 = document.getElementById("spinner2");
+  let newFragment = document.createDocumentFragment();
+  fetch(`http://localhost:5050/getFibonacciResults`).then(response => {
+    response.json().then(data => {
+      let allData = data.results;
+      let sortByDate = allData.sort((a, b) => b.createdDate - a.createdDate);
+      showAllData.innerText = " ";
+      toggleFunc(spinner2, "disappear");
+      for (let i = 0; i < allData.length; i++) {
+        let newInputVal = sortByDate[i].number;
+        let newCalcVal = sortByDate[i].result;
+        let newDate = sortByDate[i].createdDate;
+        let convertedDate = new Date(newDate).toString();
+        let newLine = `The Fibonnaci of <b> ${newInputVal} </b> is <b> ${newCalcVal} </b>. Calculated at: ${convertedDate}`;
+        let newLi = document.createElement("LI");
+        newLi.innerHTML = newLine;
+        newFragment.append(newLi);
+        addClass(showAllData, "ul-style");
+        addClass(newLi, "underline");
+      }
+      showAllData.appendChild(newFragment);
+    });
+  });
+};
 let serverFibo = () => {
   let userInput = document.getElementById("userInput");
   let showError = document.getElementById("showError");
   let userInputValue = userInput.value;
   let spinner = document.getElementById("spinner");
   let resultShown = document.getElementById("y-val");
+  let spinner2 = document.getElementById("spinner2");
   removeClass(userInput, "input-error");
   removeClass(showError, "show-error");
-  changeInnerText(showError, "");
+  showError.innerText = " ";
   if (isValidNumber(userInputValue)) {
     reinitialise();
+    toggleFunc(spinner2, "disappear");
     fetch(`http://localhost:5050/fibonacci/${userInputValue}`).then(
       response => {
         toggleFunc(spinner, "disappear");
         toggleFunc(resultShown, "disappear");
+        serverFiboResults();
         if (response.ok) {
           response.json().then(data => {
             let dataResult = data.result;
-            changeInnerText(resultShown, dataResult);
+            resultShown.innerText = dataResult;
           });
         } else {
           response.text().then(text => {
-            changeInnerText(resultShown, `Server error: ${text}`);
+            resultShown.innerText = `Server error: ${text}`;
             removeClass(resultShown, "show-result");
             addClass(resultShown, "error-message");
           });
